6c908bfc9b9eec2fc6a72164ff1d20ffdb25b543
Jaysooon <jayson.vacaro@gmail.com>
2018-06-02 12:41:12 +0800

    changes

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ sudo docker-compose up --build -d &&
 sudo docker run --rm -v $(pwd)/carmudi-server:/app composer install &&
 sudo docker exec carmuditest_app_1 chmod -R o+w storage/ &&
 sudo docker-compose exec app cp .env.example .env &&
-sudo docker-compose exec app php artisan migrate:install &&
+#sudo docker-compose exec app php artisan migrate:install &&
 sudo docker-compose exec app php artisan migrate:fresh --seed
 
